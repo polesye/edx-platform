cdca83ddc0be0d3d6449e7ddaf082da43cacabd3
Nimisha Asthagiri <nasthagiri@edx.org>
2016-01-25 10:40:35 -0500

    Mobile API: remove unused social facebook endpoints.

--- lms/djangoapps/mobile_api/urls.py
+++ lms/djangoapps/mobile_api/urls.py
@@ -13,9 +13,3 @@ urlpatterns = patterns(
     url(r'^video_outlines/', include('mobile_api.video_outlines.urls')),
     url(r'^course_info/', include('mobile_api.course_info.urls')),
 )
-
-if settings.FEATURES["ENABLE_MOBILE_SOCIAL_FACEBOOK_FEATURES"]:
-    urlpatterns += (
-        url(r'^social/facebook/', include('mobile_api.social_facebook.urls')),
-        url(r'^settings/', include('mobile_api.social_facebook.preferences.urls')),
-    )
